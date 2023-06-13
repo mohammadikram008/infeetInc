202e7f05e534373e05ee41a24164aebeb67ccf89
AppDev8890 <hannsg889@gmail.com>
2023-06-13 17:16:03 +0500

    create new feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import React, { Suspense } from "react";
 
 import Navbar from './components/Navbar'
 import Index from './components/index'
-import React, { Suspense } from "react";
+import Fallback from './components/Fallback'
 const AboutComponent = React.lazy(() => import('./components/index'));
 function App() {
 
 
   return (
     <div className="App">
-      <Suspense fallback={<div> Please Wait... </div>} >
+      {/* <Fallback/> */}
+      <Suspense fallback={<Fallback/>} >
         <AboutComponent />
         </Suspense>
 
